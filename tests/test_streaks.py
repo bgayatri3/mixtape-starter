22c76a5a8a270b5b5077c1c17098c73f2635706f
bgayatri3 <gaya3fr>
2026-07-05 09:53:17 -0400

    typo in comment submission/test

--- tests/test_streaks.py
+++ tests/test_streaks.py
@@ -102,7 +102,7 @@ def test_streak_increments_on_monday(app, user):
     with app.app_context():
         u = db.session.get(User, user.id)
         sunday = datetime(2024, 6, 16, 12, 0, 0, tzinfo=timezone.utc)  # weekday() == 6
-        monday = datetime(2024, 6, 17, 12, 0, 0, tzinfo=timezone.utc)  # weekday() == 7
+        monday = datetime(2024, 6, 17, 12, 0, 0, tzinfo=timezone.utc)  # weekday() == 0
 
         update_listening_streak(u, sunday)
         assert u.listening_streak == 1
